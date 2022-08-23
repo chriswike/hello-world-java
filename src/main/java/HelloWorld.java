0f8ab1b9c13c0d67454b47380016f33aa0b9ce3e
Chris Wike <chriswike@gmail.com>
2022-08-23 02:21:43 -0500

    added comments for demo

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,13 +2,16 @@
  * Created by LaunchCode
  */
 public class HelloWorld {
-
+    //This is a main() method. The compiler will always start at main().
+    //It also accepts a single argument of type String array.
     public static void main(String[] args) {
+        //This method also contains the System.out.println that sends text to the terminal.
+        //As well as calling the sayHello method below.
         System.out.println(sayHello());
     }
 
     public static String sayHello() {
-        //Added hello world to the return statement.
+        //This return statement sends our Hello, World! string to line 10
         return "Hello, World!";
     }
 }
